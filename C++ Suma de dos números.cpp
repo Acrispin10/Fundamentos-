9a34722333c42b97bab9c17d61aa71a83baf2830
Acrispin10 <69353160+Acrispin10@users.noreply.github.com>
2020-08-14 09:53:47 -0400

    Add files via upload

---
+++ C++ Suma de dos números.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int  a,b,c;
+    cout<<"Programa que suma dos nùmeros "<<endl;
+    cout<<"Ingrese el primer operando:";
+    cin>>a;
+    cout <<"Ingrese el segundo operand: ";
+    cin >>b;
+    c=a+b;
+    cout<<"el resultado es :"<<c;
+    return 0;
+}
